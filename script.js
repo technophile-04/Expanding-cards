5a51c5a2cfa6018f19514073732db6c143ada073
Shiv Bhonde <shivbhonde04@gmail.com>
2021-05-03 20:53:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel)=>{
+    panel.addEventListener("click", ()=>{
+        removeActiveClasses();
+        panel.classList.add("active");
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel)=>{
+        if(panel.classList.contains("active"))
+        {
+            panel.classList.remove("active");
+        }
+    })
+}
